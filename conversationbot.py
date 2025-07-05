8fb5d5c7ba13641fce92a4dc6d60fc9366c72423
aleksandrmajlo <aleksandrmajlo@gmail.com>
2025-07-05 19:46:20 +0300

    fly 4

--- conversationbot.py
+++ conversationbot.py
@@ -29,9 +29,9 @@ async def start(update: Update, context: ContextTypes.DEFAULT_TYPE) -> int:
     reply_keyboard = [["Boy", "Girl", "Other"]]
 
     await update.message.reply_text(
-        "Hi! My name is Professor Bot. I will hold a conversation with you. "
-        "Send /cancel to stop talking to me.\n\n"
-        "Are you a boy or a girl?",
+        "Привіт! Мене звати Преподавач Бот. Я буду зберігати зв'язок з тобою. "
+        "Ви можете відправити мені /cancel, щоб зупинити розмову.\n\n"
+        "Ви хто?",
         reply_markup=ReplyKeyboardMarkup(
             reply_keyboard, one_time_keyboard=True, input_field_placeholder="Boy or Girl?"
         ),
@@ -44,8 +44,8 @@ async def gender(update: Update, context: ContextTypes.DEFAULT_TYPE) -> int:
     user = update.message.from_user
     logger.info("Gender of %s: %s", user.first_name, update.message.text)
     await update.message.reply_text(
-        "I see! Please send me a photo of yourself, "
-        "so I know what you look like, or send /skip if you don't want to.",
+        "Я бачу! Будь ласка, відправь мені фото, "
+        "щоб я знала, що ви виглядаєте, або відправь /skip, якщо ви не хочете.",
         reply_markup=ReplyKeyboardRemove(),
     )
 
@@ -59,7 +59,8 @@ async def photo(update: Update, context: ContextTypes.DEFAULT_TYPE) -> int:
     await photo_file.download_to_drive("user_photo.jpg")
     logger.info("Photo of %s: %s", user.first_name, "user_photo.jpg")
     await update.message.reply_text(
-        "Gorgeous! Now, send me your location please, or send /skip if you don't want to."
+        "Красиво! Тепер відправь мені свою локацію, "
+        "або відправь /skip, якщо ви не хочете."
     )
 
     return LOCATION
@@ -70,7 +71,8 @@ async def skip_photo(update: Update, context: ContextTypes.DEFAULT_TYPE) -> int:
     user = update.message.from_user
     logger.info("User %s did not send a photo.", user.first_name)
     await update.message.reply_text(
-        "I bet you look great! Now, send me your location please, or send /skip."
+        "Ти виглядаєш великолепно! Тепер відправь мені свою локацію, "
+        "або відправь /skip, якщо ви не хочете."
     )
 
     return LOCATION
@@ -84,7 +86,7 @@ async def location(update: Update, context: ContextTypes.DEFAULT_TYPE) -> int:
         "Location of %s: %f / %f", user.first_name, user_location.latitude, user_location.longitude
     )
     await update.message.reply_text(
-        "Maybe I can visit you sometime! At last, tell me something about yourself."
+        "Можливо, я можу відвідати вас пізніше! Накінець, поділіться зі свідомою про собі."
     )
 
     return BIO
@@ -95,7 +97,7 @@ async def skip_location(update: Update, context: ContextTypes.DEFAULT_TYPE) -> i
     user = update.message.from_user
     logger.info("User %s did not send a location.", user.first_name)
     await update.message.reply_text(
-        "You seem a bit paranoid! At last, tell me something about yourself."
+        "Ти здається трохи паранійний! Накінець, поділіться зі свідомою про собі."
     )
 
     return BIO
@@ -105,7 +107,7 @@ async def bio(update: Update, context: ContextTypes.DEFAULT_TYPE) -> int:
     """Stores the info about the user and ends the conversation."""
     user = update.message.from_user
     logger.info("Bio of %s: %s", user.first_name, update.message.text)
-    await update.message.reply_text("Thank you! I hope we can talk again some day.")
+    await update.message.reply_text("Дякую! Надіюсь, ми зможемо поспілкуватися пізніше.")
 
     return ConversationHandler.END
 
@@ -115,7 +117,7 @@ async def cancel(update: Update, context: ContextTypes.DEFAULT_TYPE) -> int:
     user = update.message.from_user
     logger.info("User %s canceled the conversation.", user.first_name)
     await update.message.reply_text(
-        "Bye! I hope we can talk again some day.", reply_markup=ReplyKeyboardRemove()
+        "До побачення! Надіюсь, ми зможемо поспілкуватися пізніше.", reply_markup=ReplyKeyboardRemove()
     )
 
     return ConversationHandler.END
